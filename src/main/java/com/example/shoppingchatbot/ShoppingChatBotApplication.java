1b978b12b8c7b5cb5410a1ee9618a9413d8f61f6
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2025-03-24 17:46:33 +0900

    Initial Commit

---
+++ src/main/java/com/example/shoppingchatbot/ShoppingChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingChatBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingChatBotApplication.class, args);
+    }
+
+}
